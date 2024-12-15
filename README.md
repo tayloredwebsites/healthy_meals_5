5ce0869c5967646a011bc5169e8135303f537a5e
Dave Taylor of Taylored Web Sites <tayloredwebsites@me.com>
2024-12-15 15:18:56 -0500

    Initial commit after customizing the Lithium starter project

--- README.md
+++ README.md
@@ -1,23 +1,8 @@
-# Lithium: A Django-Powered Boilerplate
-Lithium is a batteries-included Django starter project with everything you need to start coding, including user authentication, static files, default styling, debugging, DRY forms, custom error pages, and more.
-
-> This project was formerly known as _DjangoX_ but was renamed to _Lithium_ in November 2024.
-
-https://github.com/wsvincent/djangox/assets/766418/a73ea730-a7b4-4e53-bf51-aa68f6816d6a
-
-## 👋 Free Newsletter
-[Sign up for updates](https://buttondown.com/lithiumsaas) to the free and upcoming premium SaaS version!
+# healthy_meals: A Django-Powered Diet Support Website to help make and eat healthier meals to meet your goals
 
 ## 🚀 Features
-
+- Built upon the Lithium Django Starter Project ([Lithium README](./LITHIUM_README.md))
 - Django 5.1 & Python 3.12
-- Installation via [Pip](https://pypi.org/project/pip/) or [Docker](https://www.docker.com/)
-- User authentication--log in, sign up, password reset--via [django-allauth](https://github.com/pennersr/django-allauth)
-- Static files configured with [Whitenoise](http://whitenoise.evans.io/en/stable/index.html)
-- Styling with [Bootstrap v5](https://getbootstrap.com/)
-- Debugging with [django-debug-toolbar](https://github.com/jazzband/django-debug-toolbar)
-- DRY forms with [django-crispy-forms](https://github.com/django-crispy-forms/django-crispy-forms)
-- Custom 404, 500, and 403 error pages
 
 ## Table of Contents
 * **[Installation](#installation)**
@@ -29,11 +14,11 @@ https://github.com/wsvincent/djangox/assets/766418/a73ea730-a7b4-4e53-bf51-aa68f
 * [License](#license)
 
 ## 📖 Installation
-Lithium can be installed via Pip or Docker. To start, clone the repo to your local computer and change into the proper directory.
+Healthy Meals can be installed via Pip or Docker. To start, clone the repo to your local computer and change into the proper directory.
 
 ```
-$ git clone https://github.com/wsvincent/lithium.git
-$ cd lithium
+$ git clone https://github.com/tayloredwebsites/healthy_meals.git
+$ cd healthy_meals
 ```
 
 ### Pip
@@ -64,54 +49,9 @@ Then install all packages hosted in `requirements.txt` and run `migrate` to conf
 
 ### Docker
 
-To use Docker with PostgreSQL as the database update the `DATABASES` section of `django_project/settings.py` to reflect the following:
-
-```python
-# django_project/settings.py
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
-    }
-}
-```
-
-The `INTERNAL_IPS` configuration in `django_project/settings.py` must be also be updated:
-
-```python
-# config/settings.py
-# django-debug-toolbar
-import socket
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
-```
-
-And then proceed to build the Docker image, run the container, and execute the standard commands within Docker.
-
-```
-$ docker compose up -d --build
-$ docker compose exec web python manage.py migrate
-$ docker compose exec web python manage.py createsuperuser
-# Load the site at http://127.0.0.1:8000 or http://127.0.0.1:8000/admin for the admin
-```
-
 ## Next Steps
 
-- Add environment variables. There are multiple packages but I personally prefer [environs](https://pypi.org/project/environs/).
-- Add [gunicorn](https://pypi.org/project/gunicorn/) as the production web server.
-- Update the [EMAIL_BACKEND](https://docs.djangoproject.com/en/4.0/topics/email/#module-django.core.mail) and connect with a mail provider.
-- Make the [admin more secure](https://opensource.com/article/18/1/10-tips-making-django-admin-more-secure).
-- `django-allauth` supports [social authentication](https://django-allauth.readthedocs.io/en/latest/providers.html) if you need that.
-
-I cover all of these steps in tutorials and premium courses over at [LearnDjango.com](https://learndjango.com).
-
 ## 🤝 Contributing
-
-Contributions, issues and feature requests are welcome! See [CONTRIBUTING.md](https://github.com/wsvincent/djangox/blob/master/CONTRIBUTING.md).
 
 ## ⭐️ Support
 
@@ -119,4 +59,5 @@ Give a ⭐️  if this project helped you!
 
 ## License
 
-[The MIT License](LICENSE)
+Copyright (C) 2024 David A. Taylor of Taylored Web Sites (tayloredwebsites.com)
+Licensed under [AGPL-3.0-only](https://opensource.org/license/agpl-v3/)
---
+++ static/js/base.js
@@ -0,0 +1,119 @@
+/* static/base.css */
+
+/* *************************************************************************************
+window onload event handler function to unobtrusively link javascript events to javascript functions
+unobtrusive - no javascript is needed or being obtrusive in the HTML for javascript to function in the page
+html page should have a script tag in the header that looks like:
+  <script>
+    window.onload = pageOnLoad;
+  </script>
+*/
+function pageOnLoad() {
+    // This function sets all of the event listeners for the elements on the web page
+    // There should normally be only one window on load event handler, so combining of them may be necessary
+    // event listeners are set here, attaching functions defined below
+
+    // set the onclick event on the header element of all expandCollapse article element
+    document.querySelectorAll(".expandCollapse").forEach(
+      el => {
+        console.log("element with the expandCollapse class: ".concat(el.tagName))
+        // el should be an article element, where the expandCollapse class was added
+        const children = el.children;
+        const head = el.querySelector("header");
+        if (head !== null) {
+          console.log("header for the expandCollapse class: ".concat(head.tagName))
+          head.addEventListener("click", (event) => expanderCollapser(event))
+        } else {
+          console.log("header is missing under expandCollapse class")
+        }
+        // el.addEventListener("click", (event) => expanderCollapser(event))
+      }
+    )
+
+    // prevent page reloads after clicking on a tags with target _blank
+    document.querySelectorAll("a").forEach(
+      el => el.addEventListener("click", (event) => blankATags(event))
+    )
+  }
+
+
+  /* *************************************************************************************
+  unobtrusive javascript event functions called by the window onload event handler function.
+  see riceHullerOnLoad function called in html script tag, and at the top of this document
+    - set the event handlers in the onLoad function to call the functions defined below
+  */
+
+  // expands or collapses the section of an article when the header is clicked
+  function expanderCollapser(event) {
+    // console.log("Clicked a header element with an article with the expandCollapse class")
+    const elem = event.currentTarget;
+    // console.log("called expanderCollapser on element with with tag: ".concat(elem.tagName));
+    const parent = elem.parentElement;
+    // console.log("parent with expanderCollapser on element with with tag: ".concat(parent.tagName));
+    // console.log("ARTICLE element classList: ".concat(parent.classList));
+    const wasExpanded = parent.classList.contains("expanded");
+    // console.log("parent element was element initially expanded: ".concat(wasExpanded.toString()));
+
+    if (wasExpanded === true) {
+      parent.classList.remove("expanded");
+    } else {
+      parent.classList.add("expanded");
+    }
+  };
+
+  // prevent page refresh when clicking on an a tag with a _blank target
+  function blankATags(event) {
+    // console.log("Clicked an element with the a tag")
+    const elem = event.currentTarget;
+    // console.log("called blankATags on element with with tag: ".concat(elem.tagName));
+    const href = (elem.hasAttribute("href") === true) ? elem.getAttribute("href") : "";
+    // console.log("href: ".concat(href.toString()));
+    const blankTarget = elem.hasAttribute("target") && elem.getAttribute("target") === "_blank";
+    // console.log("blankTarget: ".concat(blankTarget.toString()));
+    if (blankTarget === true && href !== "" && href !== "#") {
+      // console.log("open window for: ".concat(href.toString()));
+      window.open(href)
+      event.preventDefault();
+      event.stopPropagation();
+      }
+  };
+
+
+
+  /* *************************************************************************************
+  (global) constants
+  */
+
+  // const MY_CONSTANT = "some-value";
+
+  /* *************************************************************************************
+  helper methods
+  */
+
+  function setErrorMsg(errMsg) {
+    errElem = getById("errorMessage");
+    if (errElem === null) {
+      errElem.textContent=errMsg;
+      console.log(errMsg);
+    } else {
+      console.log("ERROR: Unable to access element with ID: 'errorMessage'");
+      console.log("ERROR: Unable to display error message: " + errMsg);
+    }
+  }
+
+  /* *************************************************************************************
+  called functions
+  */
+
+  function setFontSize(fontSize) {
+    htmlElems = document.getElementsByTagName("html");
+    if (htmlElems.length == 1) {
+      htmlElems[0].style.fontSize = fontSize;
+    } else {
+      setErrorMsg("ERROR: unable to find the 'html' element!");
+    }
+  }
+
+  function isMobileDevice() {
+    /android|iphone|kindle|ipad/i.test(navigator.userAgent)
+  }
